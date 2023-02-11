c0083ef7dcd7a1e795cd23c10c487e1b873b1528
D4rkisek <maruf.f@hotmail.com>
2023-02-11 22:37:30 +0000

    added more functionalities to the button class

--- Game.py
+++ Game.py
@@ -5,15 +5,21 @@ import button
 pygame.init()
 
 # Set screen size and title
-SCREEN_WIDTH = 1240
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Main menu")
+pygame.display.set_caption("Catan")
 ####################################################################
-# load the background image
+# load the background image for the main menu
 background = pygame.image.load('landscape.jpg').convert_alpha()
 # Scale the background image to fit the screen size
-background = pygame.transform.scale(background, (1240, 800))
+background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# load the icon for the game
+icon = pygame.image.load('icon.png').convert_alpha()
+# add the icon
+pygame.display.set_icon(icon)
+
 
 # load button images
 start_img = pygame.image.load('Play.png').convert_alpha()
@@ -21,8 +27,8 @@ quit_img = pygame.image.load('Quit.png').convert_alpha()
 start_img2 = pygame.image.load('Play2.png').convert_alpha()
 quit_img2 = pygame.image.load('Quit2.png').convert_alpha()
 # create button instance
-start_button = button.Button(250, 350, start_img, start_img2)
-quit_button = button.Button(670, 350, quit_img, quit_img2)
+start_button = button.Button(SCREEN_WIDTH/5, SCREEN_HEIGHT/2.5, start_img, start_img2)
+quit_button = button.Button(SCREEN_WIDTH/1.8, SCREEN_HEIGHT/2.5, quit_img, quit_img2)
 
 ######################################################################
 # Run the game loop
